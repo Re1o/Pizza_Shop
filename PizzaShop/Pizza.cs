39703bf05fe5c0ed7e2a4934bc59c38e8fb5940d
Re1o <reio.reimers@hotmail.com>
2020-03-02 19:15:59 +0200

    03.02.2020

---
+++ PizzaShop/Pizza.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaShop
+{
+    class Pizza
+    {
+        List<Article> InStock;
+
+
+        public menue()
+        {
+
+        }
+    }
+}
---
+++ PizzaShop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PizzaShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
